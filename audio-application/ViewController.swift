2ea8122d8fece206d01ca1f54fc601ef801456b4
Jason Shultz <jasshultz@gmail.com>
2015-10-19 09:01:55 -0600

    init commit

--- audio-application/ViewController.swift
+++ audio-application/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        player.volume = slider.value
+    }
+    
+    
+    @IBAction func playButton(sender: AnyObject) {
+        
+        player.play()
+        
+    }
+    
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+    
+
+    
+    
+    var player:AVAudioPlayer = AVAudioPlayer() // Create a player
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("mozart-k311-3-sasaki", ofType: "mp3")! // path to audio file
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            
+        } catch {
+            print("siomething went wrong")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
